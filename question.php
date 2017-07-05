31e3587ef57f60fa1a05568bc47849cb29712749
Tanmoy Das <tanmoydas465@gmail.com>
2017-07-05 13:30:22 +0200

    Update question.php

--- question.php
+++ question.php
@@ -34,13 +34,13 @@
 <html>
 	<head>
 	<meta charset="utf-8" />
-	<title>PHP Quizzer</title>
+	<title>Your Answer Pls!!!!!</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 <body>
 	<header>
 		<div class="container">
-			<h1>PHP Quizzer</h1>
+			<h1>Your Answer Please</h1>
 		</div>
 	</header>
 	<main>
